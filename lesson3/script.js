e10520557b9e2c0251f7f7a30705f410d71c5c13
Your Name <you@example.com>
2024-02-14 19:59:13 +0200

    added

---
+++ lesson3/script.js
@@ -0,0 +1,13 @@
+function welcome() {
+    alert("ברוכים הבאים!");
+}
+
+function showMessage() {
+    console.log("ערב טוב");
+}
+
+function welcomeName() {
+    var name = document.getElementById("userName").value;
+
+    alert("ברוך הבא " + name + "!");
+}
